e6d613d831b2ba220611f12f0c306cc0aee4d6fc
Michael <lopler19@mail.ru>
2023-09-03 15:14:04 +0300

    test git_pull

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
-import React from 'react'
+import React from "react";
 import AppRouter from "./components/AppRouter";
 import Navbar from "./components/Navbar";
 import ColorContainer from "./layouts/ColorContainer";
 
-
 function App() {
   return (
     <div className="bg-white">
-        <BrowserRouter>
-            <Navbar/>
-            <div className="relative isolate px-6 pt-14 lg:px-8">
-              <AppRouter/>
-              <ColorContainer/>
-            </div>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+          <AppRouter />
+          <ColorContainer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
