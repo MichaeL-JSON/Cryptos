cf955602b1af53eb072f5733ffa711fe5d9d418c
Michael <lopler19@mail.ru>
2023-09-03 20:52:54 +0300

    deleted unnecessary return

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 
 const News = () => {
-
   return <div>News</div>;
- // comment by Michael on branch mich
-    // Page news author Vozhlak
-    //Comment
-    return (
-        <div>
-            News
-        </div>
-    );
-
+  // comment by Michael on branch mich
+  // Page news author Vozhlak
+  //Comment
 };
 
 export default News;
