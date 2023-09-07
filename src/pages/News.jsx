84a7577a62aed5091518c71467c0bfb88d1c26d9
vozhlak <vozhlakov1997@yandex.ru>
2023-09-07 20:21:07 +0600

    page news test

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const BASE_URL =
+  "https://financialmodelingprep.com/api/v3/fmp/articles?page=0&size=5&apikey=c8092cca65efe2a489a17fa00af07416";
 
 const News = () => {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(BASE_URL);
+        const body = await response.json();
+        console.log(body.content);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        } else {
+          console.log(error);
+        }
+      }
+    };
+    getData();
+  }, []);
+
   return <div>News</div>;
 };
 
