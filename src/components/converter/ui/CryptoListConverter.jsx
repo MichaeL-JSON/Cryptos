b0e3c96afe2bfd06a3cdf5ac294425e098d11172
ilya <ilya123558ilya@gmail.com>
2023-11-04 20:19:54 +0300

    update form transaction component

--- src/components/converter/ui/CryptoListConverter.jsx
+++ src/components/converter/ui/CryptoListConverter.jsx
@@ -27,54 +27,50 @@ export const CryptoListConverter = ({ className }) => {
         {
           !isLoading && data ? (
             data.map((cryptoItem, index) => (
-              <>
-                <li
-                  onClick={() =>
-                    setState(() => ({
-                      showComponent: true,
-                      activeIndex: index
-                    }))
+              <li
+                onClick={() =>
+                  setState(() => ({
+                    showComponent: true,
+                    activeIndex: index
+                  }))
+                }
+                key={index}
+                className="m-[2em_0em] cursor-pointer transition-all sm:m-[1em_0em] grid grid-cols-3 sm:grid-cols-9 w-full self-center font-medium text-gray-600 text-[0.75em] sm:text-[0.85em] md:text-[1em]"
+              >
+                <div className="mb-[0.2em] sm:m-0 col-span-3 sm:col-span-3 flex items-center font-semibold text-gray-700">
+                  <img
+                    src={cryptoItem.image}
+                    alt=""
+                    className="w-5 h-5 sm:w-4 sm:h-4 md:w-5 md:h-5"
+                  />
+                  <p className="ml-[0.5em] ">{cryptoItem.name.split(" ")[0]}</p>
+                  <p className="ml-[0.4em] text-gray-500 text-[0.7em] sm:text-[0.65em] md:text-[0.8em]">
+                    {cryptoItem.symbol.toUpperCase()}
+                  </p>
+                </div>
+                <p className="ml-[2em] sm:m-0 sm:col-span-2">
+                  ${formatNumberWithSpaces(cryptoItem.current_price)}
+                </p>
+                <p
+                  className={
+                    "sm:col-span-2 text-center " +
+                    (String(cryptoItem.price_change_percentage_24h)[0] === "-"
+                      ? "text-red-700"
+                      : "text-green-600")
                   }
-                  key={index}
-                  className="m-[2em_0em] cursor-pointer transition-all sm:m-[1em_0em] grid grid-cols-3 sm:grid-cols-9 w-full self-center font-medium text-gray-600 text-[0.75em] sm:text-[0.85em] md:text-[1em]"
                 >
-                  <div className="mb-[0.2em] sm:m-0 col-span-3 sm:col-span-3 flex items-center font-semibold text-gray-700">
-                    <img
-                      src={cryptoItem.image}
-                      alt=""
-                      className="w-5 h-5 sm:w-4 sm:h-4 md:w-5 md:h-5"
-                    />
-                    <p className="ml-[0.5em] ">
-                      {cryptoItem.name.split(" ")[0]}
-                    </p>
-                    <p className="ml-[0.4em] text-gray-500 text-[0.7em] sm:text-[0.65em] md:text-[0.8em]">
-                      {cryptoItem.symbol.toUpperCase()}
-                    </p>
-                  </div>
-                  <p className="ml-[2em] sm:m-0 sm:col-span-2">
-                    ${formatNumberWithSpaces(cryptoItem.current_price)}
-                  </p>
-                  <p
-                    className={
-                      "sm:col-span-2 text-center " +
-                      (String(cryptoItem.price_change_percentage_24h)[0] === "-"
-                        ? "text-red-700"
-                        : "text-green-600")
-                    }
-                  >
-                    {cryptoItem.price_change_percentage_24h.toFixed(2)}%
-                  </p>
-                  <p className="sm:col-span-2 text-end">
-                    {cryptoItem.market_cap.length > 6
-                      ? "$" + formatNumberWithSpaces(cryptoItem.market_cap)
-                      : "$" +
-                        formatNumberWithSpaces(
-                          Math.floor(cryptoItem.market_cap / 1000000)
-                        ) +
-                        "M"}
-                  </p>
-                </li>
-              </>
+                  {cryptoItem.price_change_percentage_24h.toFixed(2)}%
+                </p>
+                <p className="sm:col-span-2 text-end">
+                  {cryptoItem.market_cap.length > 6
+                    ? "$" + formatNumberWithSpaces(cryptoItem.market_cap)
+                    : "$" +
+                      formatNumberWithSpaces(
+                        Math.floor(cryptoItem.market_cap / 1000000)
+                      ) +
+                      "M"}
+                </p>
+              </li>
             ))
           ) : (
             <>loading</>
